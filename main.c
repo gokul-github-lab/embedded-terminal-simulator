d1a3e6c71afd0eb59ae4cbf12fab2068f7527f60
ganesh_gokul <ganeshgokul599@gmail.com>
2026-06-23 20:16:25 +0530

    Implement terminal loop and exit functionality

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+    char command[100];
+    printf("Embedded Terminal v1.0\n");
+    while (1) {
+        printf("> ");
+        scanf("%99s", command);
+        if (strcmp(command, "exit") == 0) {
+            printf("Closing terminal...\n");
+            break;
+        }
+    }
+    return 0;
+}
