d1da26627a369c1828cb9b443f6727238ad06cba
ganesh_gokul <ganeshgokul599@gmail.com>
2026-06-24 19:14:56 +0530

    Add LED state management and status command
    Added LED control commands to the embedded terminal.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include<string.h>
 int main(){
 char b[100];
+int s = 0;
 printf("Embedded Terminal v1.0\n");
 while(1){
 printf("> ");
@@ -13,6 +14,22 @@ if(strcmp(b,"exit")==0){
 }
 else if(strcmp(b,"help")==0){
   printf("Available commands\nhelp\nled on\nled off\nstatus\ntemp\nhistory\nexit\n");
+}
+else if(strcmp(b,"led on")==0){
+    s = 1;
+    printf("LED ON\n");
+}
+else if(strcmp(b,"led off")==0){
+    s = 0;
+   printf("LED OFF\n");
+}
+else if(strcmp(b,"status")==0){
+   if(s==1){
+    printf("LED ON\n");
+  }
+  else{
+    printf("LED OFF\n");
+  }
 }
 else{
   printf("unknown command\n");
