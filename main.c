3e24103c1b7d8c9cc619534647922c6a58c7853f
ganesh_gokul <ganeshgokul599@gmail.com>
2026-06-24 18:29:13 +0530

    Refactor main.c for improved input handling
    Updated terminal program to use fgets for input and added help command.

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char command[100];
-    printf("Embedded Terminal v1.0\n");
-    while (1) {
-        printf("> ");
-        scanf("%99s", command);
-        if (strcmp(command, "exit") == 0) {
-            printf("Closing terminal...\n");
-            break;
-        }
-    }
-    return 0;
+#include<stdio.h>
+#include<string.h>
+int main(){
+char b[100];
+printf("Embedded Terminal v1.0\n");
+while(1){
+printf("> ");
+fgets(b,sizeof(b),stdin);
+b[strcspn(b, "\n")] = '\0';
+if(strcmp(b,"exit")==0){
+  printf("closing terminal...\n");
+  break;
+}
+else if(strcmp(b,"help")==0){
+  printf("Available commands\nhelp\nled on\nled off\nstatus\ntemp\nhistory\nexit\n");
+}
+else{
+  printf("unknown command\n");
+}
+}
+return 0;
 }
