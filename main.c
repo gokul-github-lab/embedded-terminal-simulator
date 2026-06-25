b3c6559cd46586bd90ac217d2df25662d93ed41c
ganesh_gokul <ganeshgokul599@gmail.com>
2026-06-25 11:25:31 +0530

    Add command history feature
    Added functionality to store command history in array 'a'.

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<string.h>
 int i = 25;
-int random();
 int random(){
   if(i >= 25 && i <= 45){
     i++;
@@ -13,7 +12,8 @@ int random(){
 }
 int main(){
 char b[100];
-int s = 0;
+char a[100];
+int s = 0,k=0;
 printf("Embedded Terminal v1.0\n");
 while(1){
 printf("> ");
@@ -25,14 +25,35 @@ if(strcmp(b,"exit")==0){
 }
 else if(strcmp(b,"help")==0){
   printf("Available commands\nhelp\nled on\nled off\nstatus\ntemp\nhistory\nexit\n");
+  int x= strlen(b);
+  for(int i =0 ;i< x; i++){
+   a[k] = b[i]; 
+   k++;
+   }
+   a[k] = '\n';
+   k++;
 }
 else if(strcmp(b,"led on")==0){
     s = 1;
     printf("LED ON\n");
+    int x= strlen(b);
+  for(int i =0 ;i< x; i++){
+   a[k] = b[i]; 
+   k++;
+   }
+    a[k] = '\n';
+   k++;
 }
 else if(strcmp(b,"led off")==0){
     s = 0;
    printf("LED OFF\n");
+   int x= strlen(b);
+  for(int i =0 ;i< x; i++){
+   a[k] = b[i]; 
+   k++;
+   }
+    a[k] = '\n';
+   k++;
 }
 else if(strcmp(b,"status")==0){
    if(s==1){
@@ -41,8 +62,25 @@ else if(strcmp(b,"status")==0){
   else{
     printf("LED OFF\n");
   }
+  int x= strlen(b);
+  for(int i =0 ;i< x; i++){
+   a[k] = b[i]; 
+   k++;
+   }
+    a[k] = '\n';
+   k++;
 }else if(strcmp(b,"temp")==0){
   printf("%d C\n",random());
+  int x= strlen(b);
+  for(int i =0 ;i< x; i++){
+   a[k] = b[i]; 
+   k++;
+   }
+    a[k] = '\n';
+   k++;
+}
+else if(strcmp(b,"history")==0){
+  printf("%s\n",a);
 }
 else{
   printf("unknown command\n");
